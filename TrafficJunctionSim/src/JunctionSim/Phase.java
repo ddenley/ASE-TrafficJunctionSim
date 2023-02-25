3f80c87cf112b11af4af8f2a6c515595035a8653
Daniel <daniel-denley@live.co.uk>
2023-02-25 17:04:37 +0000

    Added init of the phases from vehicles hash map, this includes a quick testing method for debugging which will be removed later

--- TrafficJunctionSim/src/JunctionSim/Phase.java
+++ TrafficJunctionSim/src/JunctionSim/Phase.java
@@ -6,12 +6,11 @@ import java.util.Queue;
 public class Phase {
 	
 	private float phaseDuration;
-	private String phaseName;
 	private Queue<String> vehicleKeysQueue = new LinkedList<String>();
 	
-	public Phase(String phaseName, float phaseDuration) {
-		this.phaseName = phaseName;
-		this.phaseDuration = phaseDuration;
+	public Phase(String phaseDuration) {
+		float float_duration = Float.valueOf(phaseDuration);
+		this.phaseDuration = float_duration;
 	}
 	
 	//Queue methods
@@ -26,10 +25,6 @@ public class Phase {
 	//Getters
 	public float getPhaseDuration() {
 		return this.phaseDuration;
-	}
-	
-	public String getPhaseName() {
-		return this.phaseName;
 	}
 	
 	public Queue<String> getVehicleKeysQueue(){
--- TrafficJunctionSim/src/JunctionSim/Phases.java
+++ TrafficJunctionSim/src/JunctionSim/Phases.java
@@ -1,6 +1,8 @@
 package JunctionSim;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Phases {
 	
@@ -10,23 +12,55 @@ public class Phases {
 	private Vehicles vehicles;
 	
 	public Phases(Vehicles vehicles) {
-		//Init phases information from csv file - e.g. 8-phase set-up or 4-phase
-		//Set phase headers for GUI
-		//Reference to vehicles object - stores all vehicles
-		//Populate the phases (the queues) from vehicles hash table
-	}
-	
-	private void populateFromCSV() {
+		this.vehicles = vehicles;
+		//ReadCSV.getHeaderValues("vehicles.csv");
+		// First read intersection.csv to create phasesHMap and set phaseHeaders
+		Object[] header_values = ReadCSV.getHeaderValues("intersection.csv");
+		String[] header = (String[]) header_values[0];
+		ArrayList<String[]> values = (ArrayList<String[]>) header_values[1];
+		setCSVHeader(header);
+		populateFromCSV(values);
+		//Now populate each phase queue from the vehicles hash map for initial init
+		populatePhaseQueuesFromHashTable();
 		
+		quickPhaseQueueCheck();
 	}
 	
-	private void setCSVHeader() {
-		
+	private void populateFromCSV(ArrayList<String[]> values) {
+		for (String[] phaseParams : values) {
+			//phaseParams[0] will be phase string ID
+			String phase_name = phaseParams[0];
+			//phaseParamas[1] will be phase duration
+			String phase_duration = phaseParams[1];
+			//Create phase object using phase duration - float conversion occurs within constructor
+			Phase phase = new Phase(phase_duration);
+			//Add phase to hash map
+			this.phasesHMap.putIfAbsent(phase_name, phase);
+		}
 	}
 	
-	private void populatePhasesFromHashTable() {
-		
+	private void setCSVHeader(String[] header) {
+		this.phaseHeaders = header;
 	}
 	
+	//https://stackoverflow.com/questions/5826384/java-iteration-through-a-hashmap-which-is-more-efficient
+	private void populatePhaseQueuesFromHashTable() {
+		for (Map.Entry<String, Vehicle> entry : this.vehicles.getVehiclesHashMap().entrySet())
+        {
+            String key = entry.getKey();
+            Vehicle vehicle = entry.getValue();
+            
+            String vehiclePhaseAllocation = vehicle.getEightPhaseAllocation();
+            
+            //Get correct phase object and place vehicle ID into queue
+            Phase phase = this.phasesHMap.get(vehiclePhaseAllocation);
+            phase.addQueue(key);
+        }
+	}
 	
+	public void quickPhaseQueueCheck() {
+		for (Phase phase : this.phasesHMap.values()) {
+			System.out.println(phase.getVehicleKeysQueue());
+		}
+	}
 }
